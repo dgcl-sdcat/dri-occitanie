a6c717c2fc108cc4c0ba0fd0d5f46753c83da3d6
dgcl-sdcat <80058755+dgcl-sdcat@users.noreply.github.com>
2021-04-22 13:03:37 +0200

    Add files via upload

---
+++ data/ProjetsMobilitTransportscollectifs_5.js
@@ -0,0 +1,17 @@
+var json_ProjetsMobilitTransportscollectifs_5 = {
+"type": "FeatureCollection",
+"name": "ProjetsMobilitTransportscollectifs_5",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "Thème": "Mobilité - Transports collectifs", "Projet": "Stations à hydrogène des hotels de région", "Coût": "200 000 €", "Subvention": "160 000 €" }, "geometry": { "type": "Point", "coordinates": [ 1.441344468, 43.58908112 ] } },
+{ "type": "Feature", "properties": { "Thème": "Mobilité - Transports collectifs", "Projet": "Acquisition de 15 cars GNV", "Coût": "4 900 000 €", "Subvention": "1 470 000 €" }, "geometry": { "type": "Point", "coordinates": [ 3.897794639, 43.60754391 ] } },
+{ "type": "Feature", "properties": { "Thème": "Mobilité - Transports collectifs", "Projet": "Acquistion de trains à hydrogène", "Coût": "25 000 000 €", "Subvention": "10 000 000 €" }, "geometry": { "type": "Point", "coordinates": [ 1.454980243, 43.6116972 ] } },
+{ "type": "Feature", "properties": { "Thème": "Mobilité - transports collectifs", "Projet": "Bus H2 Montpellier", "Coût": "4 700 000 €", "Subvention": "2 350 000 €" }, "geometry": { "type": "Point", "coordinates": [ 3.890727839, 43.60768464 ] } },
+{ "type": "Feature", "properties": { "Thème": "Mobilité - Transports collectifs", "Projet": "Pistes cyclables :- CD11 Canal du Midi\n- Muret\n- Autres pistes cyclables Toulouse", "Coût": "5 000 000 €", "Subvention": "4 000 000 €" }, "geometry": null },
+{ "type": "Feature", "properties": { "Thème": "Mobilité - Transports collectifs", "Projet": "Pistes cyclables", "Coût": "NC", "Subvention": "NC" }, "geometry": { "type": "Point", "coordinates": [ 2.351652607, 43.21247113 ] } },
+{ "type": "Feature", "properties": { "Thème": "Mobilité - Transports collectifs", "Projet": "Pistes cyclables", "Coût": "NC", "Subvention": "NC" }, "geometry": { "type": "Point", "coordinates": [ 1.333175875, 43.46273093 ] } },
+{ "type": "Feature", "properties": { "Thème": "Mobilité - Transports collectifs", "Projet": "Pistes cyclables", "Coût": "NC", "Subvention": "NC" }, "geometry": { "type": "Point", "coordinates": [ 1.440216357, 43.60181859 ] } },
+{ "type": "Feature", "properties": { "Thème": "Mobilité - Transports collectifs", "Projet": "Eco-chèques véhicules électriques aux collectivités", "Coût": "600 000 €", "Subvention": "480 000 €" }, "geometry": { "type": "Point", "coordinates": [ 3.898262907, 43.60745895 ] } },
+{ "type": "Feature", "properties": { "Thème": "Mobilité - Transports collectifs", "Projet": "Tramway de Montpellier", "Coût": "4 500 000 €", "Subvention": "2 250 000 €" }, "geometry": { "type": "Point", "coordinates": [ 3.881070658, 43.60560351 ] } }
+]
+}
